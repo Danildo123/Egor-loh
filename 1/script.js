9146ef1a84971f6d1f77f49e659afdb3660f68ed
FeedBOG <FeedBOG@mail.ru>
2023-10-28 11:58:51 +0300

    add files

---
+++ 1/script.js
@@ -0,0 +1,27 @@
+let myText = 'The dog ate my homework!';
+alert(myText);
+
+myText = 'hello, world!';
+myText = 99;
+myText = 4 * 10;
+myText = true;
+myText = undefined;
+alert(myText);
+
+
+alert(siteURL);
+siteURL = 'https://www.ya.ru';
+
+const name = 'Alex'; // string
+const age = 26; // number
+const googol = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000n; // bigint
+const isCold = false; // boolean
+const city = undefined; // undefined
+const person = { name: 'Alex', age: undefined }; // object
+const country = null; // null
+const symbol = Symbol('id'); // symbol  
+
+const currentYear = '2023';
+console.log(typeof currentYear); // string
+const numberCurrentYear = Number(currentYear); // number
+console.log(typeof numberCurrentYear);
---
+++ 3/script.js
@@ -0,0 +1,37 @@
+let firstNumber = +prompt("Введите первое число:");
+let secondNumber = +prompt("Введите второе число:");
+const operator = prompt("Введение (+, -, *, / ) :");
+
+// firstNumber = +firstNumber;
+// secondNumber = +secondNumber;
+
+let result;
+
+if (operator === "+") {
+    result = firstNumber + secondNumber;
+} else if (operator === "-") {
+    result = firstNumber - secondNumber;
+} else if (operator === "*") {
+    result = firstNumber * secondNumber;
+} else if (operator === "/") {
+    result = firstNumber / secondNumber;
+} else {
+    alert("Некорректный оператор!")
+}
+
+if (result !== undefined) {
+    alert(`Результат: ${result}`);
+}
+//const sum = firstNumber + secondNumber;
+// const difference = firstNumber - secondNumber;
+// const product = firstNumber * secondNumber;
+// const quotient = firstNumber / secondNumber;
+
+alert(
+    `Результат:
+    Сумма: ${sum},
+    Разность: ${difference},
+    Произведение: ${product},
+    Деление: ${quotient}`
+);
+
